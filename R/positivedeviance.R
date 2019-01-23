af6e7cf251f1b130d6e0fc77d83d55cc96794aff
Robert Badgett <rbadgett@kumc.edu>
2019-01-23 14:07:19 -0600

    Update positivedeviance.R

--- R/positivedeviance.R
+++ R/positivedeviance.R
@@ -1,43 +1,43 @@
-pareto <-
-function(content, topic, type, totalencounters, theme) {
-temp <- gsub('\n', '', fixed = TRUE, content, perl = TRUE)
-temp <- gsub("\\s+$", "", temp, perl = TRUE) #Removing trailing whitespace
-temp <- gsub(",+$", "", temp, perl = TRUE) #Remove trailing comma if accidentally added by user online
-temp <- paste('Mymatrix <- matrix(c(',temp,'), ncol=2, byrow=TRUE,dimnames = list(NULL, c("Reason","count")))')
-x<-eval(parse(file = "", n = NULL, text = temp))
-if (totalencounters < 0){totalencounters = 0}
-totalencounters <- as.numeric(totalencounters)
-KUBlue = "#0022B4"
-SkyBlue = "#6DC6E7"
-par(col.axis="black" ,col.lab=KUBlue ,col.main=KUBlue ,col.sub=KUBlue, col=KUBlue,new = TRUE) #bg=SkyBlue)
-if (type=="p" || type=="P")
-	{
-	count <- as.numeric(x [,2])
-	names(count) <- x [,1]
-	# 2018-04-01 removed col=SkyBlue, 
-	pareto.chart(count , ylab = "Frequency", cumperc = seq(0, 100, by = 10), xlab = "", border = KUBlue,main = "")
-	if (nchar(topic)> 1)
-		{title(topic, cex.main = 1.5, font.main= 2, col.main = KUBlue)}
-	#	{mtext(paste("Pareto analysis: ",topic), side=3, line=2, col=KUBlue , cex=3)} #Title moved here 7/19/2014}
-	#mtext("Causes of non-conformity sorted by frequency", side=3, line=0.5, col=KUBlue , cex=1.5)
-	mtext("Barriers", side=1, line=4, col=KUBlue , cex=1.5)
-	if(theme=="KU"){display_logo(x=1.2,y=0.05)}
-	}
-else
-	{
-	#plot(c(0, dev.size("px")[1]), c(0, dev.size("px")[2]),axes=F,type="b",xlab="", ylab = "", new = TRUE) # needed if rasterimage later adds anything
-	Myframe <- as.data.frame(x)
-	Myframe$count<-as.numeric(as.character(Myframe$count))
-	totalencounters <- max(Myframe$count, totalencounters)
-	ggplot(Myframe, aes(x = reorder(Reason, -count), y = count)) + 
-		geom_bar(fill = SkyBlue,stat="identity") + xlab("")+ ylab("Count") + 
-		theme(axis.title.x = element_text(size = rel(2), face="bold", angle = 0)) + 
-		theme(axis.title.y = element_text(size = rel(2), face="bold", angle = 90)) + 
-		theme(plot.background = element_rect(fill = "#FFFFFF")) + ylim(0,totalencounters) +
-		#ggtitle(expression(atop(topic, atop("Causes of non-conformity sorted by frequency", "")))) + 
-		#labs(title = "Causes of non-conformity sorted by count") + 
-		theme(plot.title = element_text(size = rel(3),face="bold",colour = KUBlue))  +
-		theme(axis.text = element_text(colour = KUBlue))+
-		theme(axis.title = element_text(colour = KUBlue))
-	}
-}
+positive.deviance <-
+  function(content, subjectlabel, outcome){
+    myframe <- data.frame (mymatrix) # For testing
+    myframe <- data.frame (content)
+    test <- 0.10
+    #test <- test * 100
+    (proportion.population = sum(myframe$numerator)/sum(myframe$denominator))
+    #(probability <- pbinom(test, 100, prob = proportion.population, lower.tail = TRUE, log.p = FALSE))
+    variance <- sum(myframe$denominator)*(proportion.population*(1-proportion.population))
+    (std.dev <- sqrt(variance))
+    (probability <- pbinom(0.1, size = 1000, prob = proportion.population, lower.tail = TRUE, log = FALSE))#  4.22 interquartile range using openmetaanalysis methods
+    # http://www.stat.yale.edu/Courses/1997-98/101/binom.htm
+    (probability <- pnorm(test, mean = proportion.population, sd = std.dev, log = FALSE))#  4.22 interquartile range using openmetaanalysis methods
+
+    # Plot
+    x <- seq(0, 1, by = 0.01)
+    densities<-dnorm(x, mean = proportion.population, sd = std.dev, log = FALSE) # *adjust
+    #x <- seq(0, 100, by = 1)
+    #densities<-dbinom(x, size = 1, prob = proportion.population , log = FALSE) # *adjust
+    plot (x, densities, type = "n", xlab=paste("Results: Percentage ",outcome,sep=""), ylab = "Probablity of result",
+          main = paste("Distribution of ",outcome," by ", subjectlabel,sep="")# 
+          ,xlim=c(0,1))#,ylim=c(0,1))
+    s <- spline(x, densities, xout=seq(0,1,by=0.01))
+    lines(s)
+    points(test,probability, col="red", pch=19)
+    
+    #Indicate local rate
+    axis(1,at=proportion.population,labels="Clinic rate", col.ticks="red", col.axis="red", col="red", las = 2)
+    
+    # Display details
+    textout1 <- paste("Probability of ",test,"% is ",round(probability,3),"%",sep="")
+    textout2 <- paste("Population size: ",sum(myframe$denominator), sep="")
+    if (proportion.population > 50){
+      text(par("usr")[1]+1*strwidth("A"),par("usr")[4]-1.5*strheight("A"),textout1,adj=c(0,0), cex=0.8)
+      text(par("usr")[1]+1*strwidth("A"),par("usr")[4]-3 * strheight("A"),textout2,adj=c(0,0), cex=0.8)
+    }else{
+      text(par("usr")[2]-1*strwidth("A"),par("usr")[4]-1.5*strheight("A"),textout1,adj=c(1,0), cex=0.8)
+      text(par("usr")[2]-1*strwidth("A"),par("usr")[4]-3 * strheight("A"),textout2,adj=c(1,0), cex=0.8)
+    }
+
+    text(par("usr")[2]/3,par("usr")[3]+(par("usr")[4]-par("usr")[3])/2,"This example is three doctors, each with 1000 patients, \nwho have outcomes rates of 10%, 15%, 20%.\nWhat population percentile is the doctor with 10%?")
+            
+  }
