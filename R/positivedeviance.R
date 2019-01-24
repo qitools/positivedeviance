99ce92873fa087ecdc40c099f2735c47575b0c0e
Robert Badgett <rbadgett@kumc.edu>
2019-01-23 22:26:04 -0600

    Update positivedeviance.R

--- R/positivedeviance.R
+++ R/positivedeviance.R
@@ -28,7 +28,7 @@ positivedeviance <- function(content, topic, outcome, outcome_type, threshold, b
 	myframe <- data.frame (x)
 	remove(x)
 	
-	#stop(paste("Dataframe rows: ",nrow(myframe), sep=""))
+	stop(paste("Dataframe rows: ",nrow(myframe), sep=""))
 
 	myframe$numerator<-as.numeric(myframe$numerator)
 	myframe$denominator<-as.numeric(myframe$denominator)
