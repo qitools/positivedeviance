d1a9f0e9c3bdb666eaa74a954161b51f95d2292e
Robert Badgett <rbadgett@kumc.edu>
2024-05-15 09:36:47 -0500

    Update positivedeviance.R

--- R/positivedeviance.R
+++ R/positivedeviance.R
@@ -372,10 +372,15 @@ meta1$studlab
 #if (data_type == "c"){stop(paste("Success so far!:\n ", 'Ready for forest plots - actual plotting: ' , x_min, ' , ', x_max, sep=""))} # Works
 # 2022-05-04 - made it to here
 
-		if (data_type %in% c("p", "c")){
+		if (data_type %in% c("p")){
 			xlim <- c(0,1)
 			leftcols=c("studlab","event","n")
 			leftlabs=c(Name_label,outcome_label,"Observations")
+			}
+		else if (data_type %in% c("c")){
+			xlim <- c(0,1)
+			leftcols=c("studlab","event")
+			leftlabs=c(Name_label,outcome_label)
 			}
 		else{
 			xlim <- c(x_min, x_max)
